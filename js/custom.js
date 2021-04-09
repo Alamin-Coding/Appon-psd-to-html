ba074e280aca3833f4768b4537e5ec4ded1ade87
Alamin-Coding <81856308+Alamin-Coding@users.noreply.github.com>
2021-04-09 22:02:15 +0600

    Added html css and js files for the project

---
+++ js/custom.js
@@ -0,0 +1,92 @@
+$(function(){
+    
+$('.screenshots_slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows:false,
+    dots:true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+//   Venobox
+$(document).ready(function(){
+    $('.venobox').venobox(); 
+  });
+
+
+
+});
+
+$(document).ready(function(){
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    // autoplay:true,
+    dots: true,
+    arrows:false,
+    centerMode: true,
+    centerPadding:false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+});
